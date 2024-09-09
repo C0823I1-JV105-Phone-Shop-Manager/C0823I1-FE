fd3b6a195e24fb96aa483fb226632077238b7fbb
Hung Levi <138595984+Long13062005@users.noreply.github.com>
2024-09-09 19:01:21 +0700

    Update index.js

--- c0823l1_fe/src/index.js
+++ c0823l1_fe/src/index.js
@@ -4,13 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
-import RouterCustom from "./Router";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterCustom />
+    <App />
   </React.StrictMode>
 
 );
