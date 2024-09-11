efb00c3201ba785fec88b96238f489b8ea8d70cd
nguyensongnghiem <143624202+nguyensongnghiem@users.noreply.github.com>
2024-09-12 01:11:44 +0700

    thêm sp

---
+++ c0823l1_fe/src/SalePage/service/itemService.jsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const searchItems = async (name) => {
+    try {
+        const token = localStorage.getItem('token');
+        console.log(token)
+        let result = await axios.get(`http://localhost:1010/api/items?name=${name}`,
+            {
+                headers: {Authorization: `Bearer ${token}`}
+            });
+        console.log(result.data)
+        return result.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
